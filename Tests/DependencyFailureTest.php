b94887a5f88eeaa33846edbe983e82cd948b779f
rovast <rovast@163.com>
2019-01-07 11:41:09 +0800

    ✨ 测试依赖

---
+++ Tests/DependencyFailureTest.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the rovast/phpunit-demo.
+ * (c) rovast <rovast@163.com>
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Rovast\PhpunitDemo\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DependencyFailureTest extends TestCase
+{
+    public function testOne()
+    {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * @depends testOne
+     */
+    public function testTwo()
+    {
+    }
+}
---
+++ Tests/MultipleDependenciesTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the rovast/phpunit-demo.
+ * (c) rovast <rovast@163.com>
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Rovast\PhpunitDemo\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class MultipleDependenciesTest extends TestCase
+{
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+
+        return 'first';
+    }
+
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+
+        return 'second';
+    }
+
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     */
+    public function testConsumer($a, $b)
+    {
+        $this->assertSame('first', $a);
+        $this->assertSame('second', $b);
+    }
+}
