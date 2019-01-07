d6ae58e880bd2ce767726b667fe8aefedce2f592
rovast <rovast@163.com>
2019-01-07 13:22:26 +0800

    ✨ add data provider

---
+++ Tests/DataTest.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the rovast/phpunit-demo.
+ * (c) rovast <rovast@163.com>
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Rovast\PhpunitDemo\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DataTest extends TestCase
+{
+    /**
+     * @dataProvider additionProvider
+     */
+    public function testAdd($a, $b, $expected)
+    {
+        $this->assertSame($expected, $a + $b);
+    }
+
+    public function additionProvider()
+    {
+        return [
+            'adding zeros' => [0, 0, 0],
+            'zero plus one' => [0, 1, 1],
+            'one plus zero' => [1, 0, 1],
+            'one plus one' => [1, 1, 3],
+        ];
+    }
+
+    public function additionProvider2()
+    {
+        return new CsvFileIterator('data.csv');
+    }
+}
---
+++ Tests/DependencyAndDataProviderComboTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the rovast/phpunit-demo.
+ * (c) rovast <rovast@163.com>
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Rovast\PhpunitDemo\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class DependencyAndDataProviderComboTest extends TestCase
+{
+    public function provider()
+    {
+        return [['provider1'], ['provider2']];
+    }
+
+    public function testProducerFirst()
+    {
+        $this->assertTrue(true);
+
+        return 'first';
+    }
+
+    public function testProducerSecond()
+    {
+        $this->assertTrue(true);
+
+        return 'second';
+    }
+
+    /**
+     * @depends testProducerFirst
+     * @depends testProducerSecond
+     * @dataProvider provider
+     */
+    public function testConsumer()
+    {
+        $this->assertSame(
+            ['provider1', 'first', 'second'],
+            func_get_args()
+        );
+    }
+}
